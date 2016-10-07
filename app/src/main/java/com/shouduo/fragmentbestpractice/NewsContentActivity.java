4bfd6884884d8461d9b97c815d5a2146bec4423e
Shouduo <handsomesaywhynot@gmail.com>
2016-10-07 23:12:08 +0800

    UI beautified and bug fixed.

--- app/src/main/java/com/shouduo/fragmentbestpractice/NewsContentActivity.java
+++ app/src/main/java/com/shouduo/fragmentbestpractice/NewsContentActivity.java
@@ -1,15 +1,15 @@
 package com.shouduo.fragmentbestpractice;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
 /**
  * Created by 刘亨俊 on 2016/5/5.
  */
-public class NewsContentActivity extends Activity {
+public class NewsContentActivity extends AppCompatActivity {
     public static void actionStart(Context context, String newsTitle, String newsContent) {
         Intent intent = new Intent(context, NewsContentActivity.class);
         intent.putExtra("news_title", newsTitle);
--- app/src/main/java/com/shouduo/fragmentbestpractice/NewsTitleFragment.java
+++ app/src/main/java/com/shouduo/fragmentbestpractice/NewsTitleFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,6 +59,7 @@ public class NewsTitleFragment extends Fragment implements AdapterView.OnItemCli
             newsContentFragment.refresh(news.getTitle(), news.getContent());
         } else {
             NewsContentActivity.actionStart(getActivity(), news.getTitle(), news.getContent());
+            Log.d("NewsTitleFragment", "onItemClick");
         }
     }
 
