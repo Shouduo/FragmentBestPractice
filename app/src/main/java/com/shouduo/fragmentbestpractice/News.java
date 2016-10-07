04a77034b5fad208275ae26404ca408cd386c5a1
Shouduo <handsomesaywhynot@gmail.com>
2016-10-07 21:43:47 +0800

    First commit

---
+++ app/src/main/java/com/shouduo/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.shouduo.fragmentbestpractice;
+
+/**
+ * Created by 刘亨俊 on 2016/5/5.
+ */
+public class News {
+
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
